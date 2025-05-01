1dcaa40d2e4f51f71a0f8b82e0b4bd326a4605f7
ngocvung90 <ngocvung.dt3.k53@gmail.com>
2025-05-01 08:37:32 +0700

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set DIR=%~dp0
-set JAVA_EXE=java
-if not "%JAVA_HOME%"=="" set JAVA_EXE=%JAVA_HOME%\bin\java
-"%JAVA_EXE%" -classpath "%DIR%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
